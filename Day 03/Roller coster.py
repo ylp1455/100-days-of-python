52fc8375b23308d55e43f072c105739b82b92b0a
Yasiru Perera <yasiruperera682@gmail.com>
2023-05-25 18:47:22 +0530

    Day 03

---
+++ Day 03/Roller coster.py
@@ -0,0 +1,7 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+if height > 120 :
+  print("You can ride the roller coster")
+else:
+  print("Sorry you cant ride the roller coster")
+  
