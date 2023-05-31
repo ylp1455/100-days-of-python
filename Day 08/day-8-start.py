dad4898613bc42ac0a7367f6338db3484861b9f3
Yasiru Perera <yasiruperera682@gmail.com>
2023-05-31 19:07:49 +0530

    Day 08

---
+++ Day 08/day-8-start.py
@@ -0,0 +1,23 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+
+
+def greet():
+    print("Hello")
+    print("Zdravstvuite")
+    print("Kon'nichiwa")
+
+def greet_with_name(name):
+    print("Hello" ,name)
+    print("Zdravstvuite " , name)
+
+
+name = input("Kak Vas zovut\n")
+
+
+
+greet_with_name(name)
+
