d73dbf2268eed1b3bc168af329acfd1084b2f770
Yasiru Perera <yasiruperera682@gmail.com>
2023-06-02 00:10:39 +0530

    Day 09

---
+++ Day 09/day-9-start.py
@@ -0,0 +1,31 @@
+programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", 
+                           "Function": "A piece of code that you can easily call over and over again."}
+
+
+
+
+
+
+print(programming_dictionary["Bug"])
+
+# addomg new item
+programming_dictionary["Loop"] = "The action of doing something over and over agin."
+
+
+
+empty_dictionary = {}
+
+# wipe dictionary
+programming_dictionary = {}
+
+# print(programming_dictionary)
+
+# eddit item in a dictionary
+programming_dictionary["Bug"] = "A moth in my computer"
+
+# print(programming_dictionary)
+
+
+# Loop thorugh a dictionary
+for key in programming_dictionary:
+    print(key)
